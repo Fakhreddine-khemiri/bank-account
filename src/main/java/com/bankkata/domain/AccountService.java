0cc3da5dd5e87c74e32e1a0e68f6a3bcb30f1e8b
fkhemiri <khemiri.fakhri@gmail.com>
2025-06-03 11:12:28 +0200

    refactor(service): Introduce AccountService to orchestrate operations and manage date

---
+++ src/main/java/com/bankkata/domain/AccountService.java
@@ -0,0 +1,34 @@
+package com.bankkata.domain;
+
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+@Service
+public class AccountService {
+
+    private final Account account;
+    private final ClockService clockService;
+
+    public AccountService(Account account, ClockService clockService) {
+        this.account = account;
+        this.clockService = clockService;
+    }
+
+    public void deposit(BigDecimal amount) {
+        account.deposit(amount, clockService.now());
+    }
+
+    public void withdraw(BigDecimal amount) {
+        account.withdraw(amount, clockService.now());
+    }
+
+    public BigDecimal getBalance() {
+        return account.getBalance();
+    }
+
+    public List<Transaction> getStatementTransactions() {
+        return account.getTransactions();
+    }
+}
