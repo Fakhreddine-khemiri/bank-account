1c9627a7cab7282e13663a82ae70720137d73bef
fkhemiri <khemiri.fakhri@gmail.com>
2025-06-04 09:05:00 +0200

    chore: add jest and define it to be used for test

---
+++ frontend/jest.config.ts
@@ -0,0 +1,16 @@
+export default {
+  preset: 'jest-preset-angular',
+  setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+      useESM: true
+    }
+  },
+  transform: {
+    '^.+\\.(ts|mjs|html|js)$': ['ts-jest', { useESM: true }]
+  },
+  moduleNameMapper: {},
+  testEnvironment: 'jsdom',
+  moduleFileExtensions: ['ts', 'html', 'js', 'json', 'mjs'],
+};
