bc51f059c6071caa7d54c50a1b967572507d587e
fkhemiri <khemiri.fakhri@gmail.com>
2025-06-04 15:58:49 +0200

    feat: add statement component to display list of all transactions

---
+++ frontend/src/app/components/statement/statement.component.spec.ts
@@ -0,0 +1,26 @@
+import { StatementComponent } from './statement.component';
+import { AccountService } from '../../service/account.service';
+import { of } from 'rxjs';
+
+describe('StatementComponent (sans TestBed)', () => {
+  let component: StatementComponent;
+  let mockAccountService: jest.Mocked<AccountService>;
+
+  beforeEach(() => {
+    mockAccountService = {
+      getStatement: jest.fn().mockReturnValue(
+          of([
+            { date: '2025-06-01', type: 'deposit', amount: 200 },
+            { date: '2025-06-02', type: 'withdrawal', amount: 50 }
+          ])
+      )
+    } as unknown as jest.Mocked<AccountService>;
+
+    component = new StatementComponent(mockAccountService);
+  });
+
+  it('should fetch and store the transactions', () => {
+    expect(component.transactions.length).toBe(2);
+    expect(component.transactions[0].type).toBe('deposit');
+  });
+});
