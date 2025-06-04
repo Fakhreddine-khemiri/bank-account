7f9fcdc6d318cdb0718232974948aaac70601a95
fkhemiri <khemiri.fakhri@gmail.com>
2025-06-04 10:05:47 +0200

    refactor: baseUrl is common to every URL.

--- frontend/src/app/service/account.service.ts
+++ frontend/src/app/service/account.service.ts
@@ -6,16 +6,17 @@ import {Observable} from "rxjs";
   providedIn: 'root',
 })
 export class AccountService {
+  private baseUrl = '/api/account';
   constructor(private http: HttpClient) {
 
   }
 
   getBalance(): Observable<number> {
-    return this.http.get<number>('/api/account/balance');
+    return this.http.get<number>(`${this.baseUrl}/balance`);
   }
 
   deposit(amount: number): Observable<string> {
-    return this.http.post('/api/account/deposit', {amount}, {responseType: 'text'});
+    return this.http.post(`${this.baseUrl}/deposit`, {amount}, {responseType: 'text'});
   }
 
 }
