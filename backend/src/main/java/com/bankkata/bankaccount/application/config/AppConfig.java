10eaa31d5593ef1d4e0b2dd43cc399d1ed7f6dcd
fkhemiri <khemiri.fakhri@gmail.com>
2025-06-04 10:44:40 +0200

    feat: add corsConfigurer Bean to prevent from cross-origin problem

--- backend/src/main/java/com/bankkata/bankaccount/application/config/AppConfig.java
+++ backend/src/main/java/com/bankkata/bankaccount/application/config/AppConfig.java
@@ -1,8 +1,11 @@
 package com.bankkata.bankaccount.application.config;
 
 import com.bankkata.bankaccount.domain.model.Account;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class AppConfig {
@@ -10,5 +13,18 @@ public class AppConfig {
     @Bean
     public Account account() {
         return new Account();
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(@NotNull CorsRegistry registry) {
+                registry.addMapping("/api/**")
+                        .allowedOrigins("http://localhost:4200")
+                        .allowedMethods("GET", "POST")
+                        .allowedHeaders("*");
+            }
+        };
     }
 }
